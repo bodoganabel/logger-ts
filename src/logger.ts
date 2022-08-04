d364d399f90eb2acd7df2b2ae656b9652bb6afc2
bodoganabel <abel.bodogan@zooshgroup.com>
2022-08-04 22:13:18 +0200

    Works

--- src/logger.ts
+++ src/logger.ts
@@ -9,22 +9,37 @@ const config = {
 
 const LOGDIR = path.join(__dirname, '..', 'logs');
 
-const logBuffer = [];
+const logBuffer: string[] = [];
 
-export function logg(source: string, message: string) {
-  const date = new Date();
+export function logg(message: string, source?: string) {
+  const dateIsoString = new Date().toISOString();
+  const dateString = dateIsoString.split('T')[0] + " " + dateIsoString.split('T')[1].split('.')[0];
   console.log(message);
-
+  logBuffer.push(`${dateString} [${source}]: ${message}\n`);
 }
 
 export function initLogWriteFile() {
-  return setInterval(() => {
+
+  //Testing
+  /*   setInterval(() => {
+      logg(makeRandomString(50), "testing");
+    }, 150);
+   */
+  return setInterval(async () => {
 
     cleanupOldLogs();
-    const todayLogFilename = getTodayLogFilename();
 
-    console.log('Writing logs to file to...', todayLogFilename);
+    const todayLogFilepath = LOGDIR + "/" + getTodayLogFilename();
+    console.log('Writing logs to file to...', todayLogFilepath);
+    for (let index = 0; index < logBuffer.length; index++) {
+      const log = logBuffer[index];
+      fs.appendFileSync(todayLogFilepath, log);
+    }
+
   }, config.saveToFileInterval);
+
+
+
 }
 
 function cleanupOldLogs() {
@@ -35,15 +50,32 @@ function cleanupOldLogs() {
     const today = Date.parse(getTodayLogFilename().split('.')[0]);
 
     if (isNaN(date) || today - config.storeLogsDays * 86400000 > date) {
+
+      const filePath = LOGDIR + "/" + logFile;
       fs.unlinkSync(LOGDIR + "/" + logFile);
+      console.log(`Removed file: ${filePath}`);
     }
+
 
   });
 }
 
 function getTodayLogFilename() {
   const now = new Date();
-  const monthString = now.getUTCMonth() < 10 ? "0" + now.getUTCMonth().toString() : now.getUTCMonth().toString();
+  const monthString = now.getUTCMonth() < 10 ? "0" + (now.getUTCMonth() + 1).toString() : (now.getUTCMonth() + 1).toString();
   const dayString = now.getUTCDate() < 10 ? "0" + now.getUTCDate().toString() : now.getUTCDate().toString();
-  return now.getUTCFullYear().toString() + "-" + monthString + "-" + dayString + ".txt";
+  const filename = now.getUTCFullYear().toString() + "-" + monthString + "-" + dayString + ".txt";
+  return filename;
+}
+
+
+function makeRandomString(length: number) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  return result;
 }
